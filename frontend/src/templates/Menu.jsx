77593457f52161d1319a37191b678b5b2578c2fc
JoaoValim <joaovitorvalimm@gmail.com>
2023-12-04 12:36:04 -0300

    update

--- frontend/src/templates/Menu.jsx
+++ frontend/src/templates/Menu.jsx
@@ -4,7 +4,7 @@ export default function Menu(props) {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand as={Link} to="/">Menu</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/BCCJoaoVitor/">Menu</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
